6ba3225b9feb227c77cd6f3be3fa7a491dddaad6
phanindra <47366677+tpnath@users.noreply.github.com>
2019-07-09 16:59:58 -0400

    Add files via upload

--- src/main/java/com/miracle/orders/OrdersController.java
+++ src/main/java/com/miracle/orders/OrdersController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,19 +21,52 @@ public class OrdersController {
 	@Autowired
 	private ProductRepository productRepository;
 	
+	@Autowired
+	private LineRepository lineRepository;
+	
 	@PostMapping("/orders/newOrder")
 	@Transactional
 	@Modifying(clearAutomatically = true)
-	public GLCC_Orders addNewOrder(@RequestBody GLCC_Orders o1) {
-		return orderRepository.save(o1);
+	public void addNewOrder(@RequestBody OrderDetails o1) {
+		GLCC_Orders order = new GLCC_Orders();
+		order.setCustId(o1.getCustId());
+		order.setDiscountPrice(o1.getDiscount());
+		order.setOrderdate(o1.getOrdereddate());
+		order.setOrderTotalPrice(o1.getOrderPrice());
+		order.setPaymentAmount(0);
+		order.setTax(o1.getTax());
+		orderRepository.save(order);
+		for(Line_Items l1 : o1.getProductDetails()) {
+			lineRepository.save(l1);
+		}
 	}
 	
 	@PutMapping("/orders/updateOrder/{orderId}")
 	@Transactional
 	@Modifying(clearAutomatically = true)
-	public GLCC_Orders updateOrder(@PathVariable(value = "orderId") String id, @RequestBody GLCC_Orders o1) {
-		return orderRepository.save(o1);
+	public void updateOrder(@PathVariable(value = "orderId") String id, @RequestBody OrderDetails o1) {
+		GLCC_Orders order = new GLCC_Orders();
+		order.setCustId(o1.getCustId());
+		order.setDiscountPrice(o1.getDiscount());
+		order.setOrderdate(o1.getOrdereddate());
+		order.setOrderTotalPrice(o1.getOrderPrice());
+		order.setPaymentAmount(0);
+		order.setTax(o1.getTax());
+		orderRepository.save(order);
+		
+		for(Line_Items l1 : o1.getProductDetails()) {
+			lineRepository.save(l1);
+		}
+		
+//		return orderRepository.save(o1);
 	}
+	
+//	@PutMapping("/orders/deleteOrder/{orderId}")
+//	public void deleteById(@PathVariable(value = "orderId") String id, @RequestBody OrderDetails o1) {
+//		
+//	}
+//	
+//	@DeleteMapping()
 	
 	@GetMapping("/orders/{custId}")
 	public List<GLCC_Orders> displayAll(@PathVariable(value = "custId") String customerId){
